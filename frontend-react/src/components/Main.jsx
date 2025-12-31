a5462bc7a9d83973387ca869a05df91c3800ae99
Ramnivasyadav <ryadav805716@gmail.com>
2025-12-31 18:07:42 +0530

    homepage react

---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <>
+    <div className='container'>
+        <div className='p-5 text-center bg-light-dark rounded'>
+        <h1 className='text-light'>Stock Prediction Portal</h1>
+        <p className='text-light lead'>AI stocks, prediction markets, and forecasting models are frequently featured in financial news, highlighting both optimism and caution around using predictive systems to guide trading decisions — including algorithm-driven forecasts for major AI players such as Nvidia, Meta, and A technology-powered platform that predicts stock behavior through advanced analytics.</p>
+        <Button text="Login" class="btn-outline-info" />
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Main
