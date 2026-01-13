b27d81de89fc3222507c171a41d31d636563eb9f
Ramnivasyadav <ryadav805716@gmail.com>
2026-01-13 16:24:12 +0530

    Authentication functionalities

--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
         <div className='p-5 text-center bg-light-dark rounded'>
         <h1 className='text-light'>Stock Prediction Portal</h1>
         <p className='text-light lead'>AI stocks, prediction markets, and forecasting models are frequently featured in financial news, highlighting both optimism and caution around using predictive systems to guide trading decisions — including algorithm-driven forecasts for major AI players such as Nvidia, Meta, and A technology-powered platform that predicts stock behavior through advanced analytics.</p>
-        <Button text="Login" class="btn-outline-info" />
+        <Button text="Explore Now" class="btn-outline-info" url="/dashboard" />
         </div>
     </div>
     </>
